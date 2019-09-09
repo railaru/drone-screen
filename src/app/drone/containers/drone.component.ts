3c5759ec825a931803016d0e0b485f1fd48d47c5
Rutenis <rutenis@luno.com>
2019-09-09 17:33:29 +0100

    updates

--- src/app/drone/containers/drone.component.ts
+++ src/app/drone/containers/drone.component.ts
@@ -9,7 +9,5 @@ export class DroneComponent implements OnInit {
 
   constructor() { }
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
--- src/app/drone/services/api.service.ts
+++ src/app/drone/services/api.service.ts
@@ -1,9 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import ToyInterface from '../interfaces/toy.interface';
-import { Observable } from 'rxjs';
-import { tap } from 'rxjs/operators';
-import {consoleTestResultHandler} from 'tslint/lib/test';
+import {Observable, of} from 'rxjs';
+import {catchError, tap} from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -16,10 +15,36 @@ export class ApiService {
 
   private apiUrl = 'https://jsonplaceholder.typicode.com/posts';
 
+
+  /**
+   * Handle Http GET operation for all toys.
+   * Let the app continue.
+   */
   fetchToys(): Observable<ToyInterface[]> {
     return this.http.get<ToyInterface[]>(this.apiUrl)
       .pipe(
-        tap(() => console.log('fetched heroes'))
+        tap(() => console.log('fetched toys')),
+        catchError(this.handleError<ToyInterface[]>('getHeroes', []))
       );
+  }
+
+  /**
+   * Handle Http operation that failed.
+   * Let the app continue.
+   * @param operation - name of the operation that failed
+   * @param result - optional value to return as the observable result
+   */
+  private handleError<T>(operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+
+      // TODO: send the error to remote logging infrastructure
+      console.error(error); // log to console instead
+
+      // TODO: better job of transforming error for user consumption
+      console.log(`${operation} failed: ${error.message}`);
+
+      // Let the app keep running by returning an empty result.
+      return of(result as T);
+    };
   }
 }
